b06d36c246e37918277b8b8343d3b1bb65a86104
Md Mahfuz Rahman Niob <njnirob2019@gmail.com>
2022-06-18 23:14:43 +0600

    For Loop Pratices

---
+++ For_Loop_Partics/1_Practice.c
@@ -0,0 +1,18 @@
+//Write a program in C to read 10 numbers from keyboard and find their sum and average.
+#include <stdio.h>
+int main()
+{       
+    int i,n,sum=0;
+	float avg;
+	printf("Input the 10 numbers : \n");
+	for (i=1;i<=10;i++)
+	{
+                printf("Number-%d :",i);
+
+		scanf("%d",&n);
+		sum +=n;
+	}
+	avg=sum/10.0;
+	printf("The sum of 10 no is : %d\nThe Average is : %f\n",sum,avg);
+ 
+}
