9f18c7a94812c246ada0c83cd91c5efdad238abe
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2022-06-25 11:55:29 +0600

    Loop

---
+++ For_Loop_Partices/2_Pratice.c
@@ -0,0 +1,19 @@
+//Write a program in C to display the n terms of odd natural number and their sum
+#include<stdio.h>
+int main()
+{
+    int i,num,sum = 0;
+
+    printf("Enter number of terms :");
+    scanf("%d",&num);
+
+    printf("\n The Odd numbers are :");
+    for(i=1;i<=num;i++)
+    {
+        printf("%d",2*i-1);
+        sum = sum+2*i-1;
+    }
+    printf("\n The sum of odd Natural number upto %d terms : %d\n",num,sum);
+return 0;
+
+}
---
+++ For_Loop_Partices/3_Pratice.c
@@ -0,0 +1,23 @@
+//Write a program in C to display the pattern like right angle triangle with a number. The pattern
+like :
+1
+12
+123
+1234*/
+#include<stdio.h>
+int main()
+{
+    int i,j,rows;
+
+    printf("Input number of rows :");
+    scanf("%d",&rows);
+
+    for(i=1;i<=rows;i++)
+    {
+        for(j=1;j<=i;j++)
+    printf("%d",j);
+    printf("\n");
+    }
+return 0;
+
+}
